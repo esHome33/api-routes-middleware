d3a770f31af43d16ed7adfbc6819858f474dc795
esHome33 <etienschwa@gmail.com>
2022-05-31 20:06:10 +0200

    visu query

--- pages/api/cookies.js
+++ pages/api/cookies.js
@@ -2,7 +2,9 @@ import cookies from '../../utils/cookies'
 
 const handler = (req, res) => {
   // The cookie middleware will add the `set-cookie` header
-  res.cookie('Next.js', 'api-middleware!')
+  const qu =  req.query
+
+  res.cookie('Next.js', 'api-middleware! '+ qu)
   
   // Return the `set-cookie` header so we can display it in the browser and show that it works!
   res.end(res.getHeader('Set-Cookie'))
